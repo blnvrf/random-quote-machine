b6346680561c68ebd79df521c9310cc6a5da4913
Blin <blnvrf@gmail.com>
2023-07-17 17:18:52 +0200

    Setting up the project
    Setting up the project with React and Tailwind

---
+++ random-quote-machine/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Header/Header'
+
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header">
+        <Header name="test"></Header>
+      </header>
+    </div>
+  );
+}
+
+export default App;
